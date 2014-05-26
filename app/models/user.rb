818ed53028965075d4444c664a2d7f8900657401
Gabriel <gabriel.c1901@gmail.com>
2014-05-26 12:20:09 -0300

    added update user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
   has_secure_password
   has_many :posts, dependent: :destroy
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates_attachment :avatar, :presence => true,
     :content_type => { :content_type => ["image/jpeg", "image/png"] },
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -71,9 +71,24 @@ describe "User pages" do
         it { should have_link('Sign out') }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
-
     end
 
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
   end
 end
 
