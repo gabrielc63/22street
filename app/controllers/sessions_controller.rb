7ddd4d32ee1fd0a065f81014dcdd1fc1850767b1
gabrielc63 <gabriel.c1901@gmail.com>
2015-12-27 01:07:54 -0300

    updated sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       sign_in user
       flash[:success] = "Welcome #{user.name}"
       return_point(user)
